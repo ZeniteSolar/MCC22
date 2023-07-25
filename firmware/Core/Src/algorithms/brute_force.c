ce3287ed74cd22150b865d81489afd51d57f10d0
alejopm03 <perdomomilar@gmail.com>
2023-07-25 12:02:20 -0300

    feat[CONTROL]: improve peo and brute force algorithms

--- firmware/Core/Src/algorithms/brute_force.c
+++ firmware/Core/Src/algorithms/brute_force.c
@@ -5,21 +5,27 @@
 #include "brute_force.h"
 #include "math.h"
 
+// Brute Force configuration 
+#define CONTROL_BRUTE_FORCE_FREQUENCY 7e3f
+
 static algorithms_metadata_t metadata;
 static float actual_duty = 0.0;
+static uint8_t done; /* Internal done */
 
 
 void brute_force_init(float initial_duty)
 {
+	control_set_freq(CONTROL_BRUTE_FORCE_FREQUENCY);
     metadata.absolute_mpp_duty = 0.0;
     metadata.absolute_mpp_power = 0.0;
     metadata.done = 0;
+	done = 0;
     actual_duty = initial_duty;
 }
 
 float brute_force_run(const volatile inputs_t *inputs)
 {
-    static float step = 0.00003;
+    static float step = 0.0003;
     float pi = inputs->v_p * inputs->i_p;
     
     if (pi > metadata.absolute_mpp_power)
@@ -27,20 +33,23 @@ float brute_force_run(const volatile inputs_t *inputs)
         metadata.absolute_mpp_power = pi;
         metadata.absolute_mpp_duty = pwm_get_duty();
     }
-
+	
 /** When sweep is done go to mppt point*/
-    if (!metadata.done)
+
+    if (!done)
         actual_duty += step;
     else
     {
         if ((int)(1000 * actual_duty) != (int)(1000 * metadata.absolute_mpp_duty))
             actual_duty -= step;
+		else
+			metadata.done = 1;
     }
 
     if (actual_duty > PWM_MAX)
     {
         actual_duty = PWM_MAX;
-        metadata.done = 1;
+        done = 1;
     }
 
     return actual_duty;
--- firmware/Core/Src/algorithms/peo.c
+++ firmware/Core/Src/algorithms/peo.c
@@ -1,8 +1,15 @@
 #include "peo.h"
+#include "math.h"
+#include "stdlib.h"
 
 static algorithms_metadata_t metadata;
+#define CONTROL_PEO_FREQUENCY 10.0f
+#define CONTROL_PEO_STEP 0.005f
+#define PEO_PWM_MAX 0.9f
+#define PEO_PWM_MIN 0.3f
 
-static float step = 0.005;
+static float step = CONTROL_PEO_STEP;
+static float dir = 1;
 static float duty;
 
 enum observations{
@@ -13,6 +20,8 @@ enum observations{
 
 void peo_init(float initial_duty)
 {
+	control_set_freq(CONTROL_PEO_FREQUENCY);
+	step = CONTROL_PEO_STEP;
     metadata.absolute_mpp_duty = 0;
     metadata.absolute_mpp_power = 0;
     metadata.done = 0;
@@ -22,25 +31,51 @@ void peo_init(float initial_duty)
 
 void peo_set_step(float step_input)
 {
+	printf("step before: %f", step);
     step = step_input;
+	printf("step after: %f\n", step);
+
 }
 
 float peo_run(const volatile inputs_t *inputs)
 {
     
     static float pi[SIZE] = {0.0, 0.0};
+    static float vi[SIZE] = {0.0, 0.0};
+	static float dpi_sum;
+	static int samples = 0;
     
-    // Compute input power
-    pi[ACTUAL] = inputs->v_p * inputs->i_p;
-    // Compute input power variation
-    float dpi = pi[ACTUAL] - pi[PREV];
-    
-    // if last power was better, invert step
-    if (dpi <= 0)
-        step = -step;
 
+    // Compute input power
+	vi[ACTUAL] = inputs->v_p;
+    pi[ACTUAL] = vi[ACTUAL] * inputs->i_p;
+
+    // Compute input power variation
+	float dvi = vi[ACTUAL] - vi[PREV];
+
+    float dpi = (pi[ACTUAL] - pi[PREV]);
+
+	// dpi_sum += dpi;
+	// samples++;
+	// if (samples >= 100)
+	// {
+	// 	// if last power was better, invert step
+	// 	static int batata = 0;
+	// 	if (dpi_sum < 0.0){
+	// 		dir = -dir;
+	// 	}
+	// 	dpi_sum = 0;
+	// 	samples = 0;
+	// }
     //Update Prev
+	//printf("a:%f p:%f d:%f s: %f\n", pi[ACTUAL], pi[PREV], dpi_sum, dir * step);
+
+	if (dpi <= 0.0){
+		dir = -dir;
+	}
+
     pi[PREV] = pi[ACTUAL];
+    vi[PREV] = vi[ACTUAL];
 
     // save metadata
     if (pi[ACTUAL] > metadata.absolute_mpp_power)
@@ -49,7 +84,15 @@ float peo_run(const volatile inputs_t *inputs)
         metadata.absolute_mpp_power = pi[ACTUAL];
     }
 
-    return duty + step;
+	duty += (dir * step);
+
+	if (duty > PEO_PWM_MAX)
+		duty = PEO_PWM_MAX;
+
+	if (duty < PEO_PWM_MIN)
+		duty = PEO_PWM_MIN;
+
+    return duty;
 }
 
 const algorithms_metadata_t *peo_get_metadata(void)
