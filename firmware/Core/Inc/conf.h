d1688bd6937fb43f85f4119de32d52433854bb10
alejopm03 <perdomomilar@gmail.com>
2023-12-26 22:25:57 -0300

    feat[adc]: change config file to ads111x configuration

--- firmware/Core/Inc/conf.h
+++ firmware/Core/Inc/conf.h
@@ -5,31 +5,19 @@
 #define ANALOG_VREF 3.0f
 
 // ADC Configuration
-#define ADC_RESOLUTION 12
-#define ADC_MAX_VALUE (1 << ADC_RESOLUTION)
-#define ADC_OVERSAMPLE 4
-#define ADC_CHANNELS 7
-#define ADC_SAMPLES 15
-#define ADC_BUFFER_SIZE ADC_CHANNELS *ADC_SAMPLES
+#define ADC_DEFAULT_FREQUENCY 200.0f // in Hz
 
 
 
 // PWM Configuration
 #define PWM_MIN 0.0f
 #define PWM_MAX 0.8f
-#define DEFAULT_PWM_FREQ 300e3f
+#define DEFAULT_PWM_FREQUENCY 300e3f
 
 
 // Control Configuration
 #define MINIMUM_POWER 20.0f // in W
 #define CONTROL_ERROR_RESET_MILLIS 3000 /* Time to wait for system stabilization when a error is triggered*/
-#define CONTROL_DEFAULT_FREQUENCY  10.0f
-
-
-
-// ADC default frequency
-#define ADC_TIM_FREQ 80e6 //ADC timer clock frequency
-#define DEFAULT_ADC_FREQ CONTROL_DEFAULT_FREQUENCY
-
+#define CONTROL_DEFAULT_FREQUENCY  1.0f
 
 #endif
