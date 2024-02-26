3f68cab7409502a1409a78bf3410572b64c57727
alejopm03 <perdomomilar@gmail.com>
2024-02-25 21:50:14 -0300

    feat: setup optimal PWM frequency

--- firmware/Core/Inc/conf.h
+++ firmware/Core/Inc/conf.h
@@ -12,10 +12,11 @@
 // PWM Configuration
 #define PWM_MIN 0.0f
 #define PWM_MAX 0.8f
-#define DEFAULT_PWM_FREQUENCY 300e3f
+#define DEFAULT_PWM_FREQUENCY 137457.0f // Optimal frequency 
 
 
 // Control Configuration
+#define CONTROL_MAXIMUM_BATTERY_VOLTAGE 55.0f // in V
 #define MINIMUM_POWER 20.0f // in W
 #define CONTROL_ERROR_RESET_MILLIS 3000 /* Time to wait for system stabilization when a error is triggered*/
 #define CONTROL_DEFAULT_FREQUENCY  1.0f
